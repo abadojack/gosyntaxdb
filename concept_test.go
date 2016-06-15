f3643193d8f514c6d6a8c78fb19dfa878f2acad6
abadojack <abadojack@gmail.com>
2016-06-15 16:23:01 +0530

    Add more tests.

--- concept_test.go
+++ concept_test.go
@@ -1,44 +1,50 @@
 package gosyntaxdb
 
 import (
+	"math/rand"
 	"net/url"
+	"strconv"
 	"testing"
+	"time"
 )
 
-// TODO: Write better tests.
-
 func Test_GetConcepts(t *testing.T) {
-	c, err := GetConcepts(nil)
+	sort := []string{"id", "concept_name", "category_id", "position", "language_id", "language_permalink",
+		"concept_search", "concept_permalink", "description", "syntax", "notes", "examples", "keywords", "related", "documentation",
+		"-id", "-concept_name", "-category_id", "-position", "-language_id", "-language_permalink",
+		"-concept_search", "-concept_permalink", "-description", "-syntax", "-notes", "-examples", "-keywords", "-related", "-documentation"}
+
+	r := rand.New(rand.NewSource(time.Now().Unix()))
+
+	v := url.Values{}
+	v.Set("limit", strconv.Itoa(1+r.Intn(20)))
+	v.Set("sort", sort[r.Intn(len(sort)-1)])
+
+	concepts, err := GetConcepts(v)
 	if err != nil {
-		t.Fatal(err)
-	} else {
-		t.Log(c)
+		t.Fatal("GetConcepts", err)
+	}
+
+	concept, err := GetConcept(concepts[r.Intn(len(concepts)-1)].ID, nil)
+	if err != nil {
+		t.Fatal("GetConcept", err)
+	}
+
+	_, err = GetLanguageConcept(concept.LanguagePermalink, concept.Permalink, nil)
+	if err != nil {
+		t.Fatal("GetLanguageConcept", err)
 	}
 }
 
 func Test_SearchConcepts(t *testing.T) {
-	c, err := SearchConcepts(url.Values{"q": {"arrays"}})
-	if err != nil {
-		t.Fatal(err)
-	} else {
-		t.Log(c)
-	}
-}
+	q := []string{"arrays", "variables", "string"}
+	r := rand.New(rand.NewSource(time.Now().Unix()))
 
-func Test_GetConcept(t *testing.T) {
-	c, err := GetConcept(52, nil)
-	if err != nil {
-		t.Fatal(err)
-	} else {
-		t.Log(c)
-	}
-}
+	v := url.Values{}
+	v.Set("q", q[r.Intn(len(q)-1)])
 
-func Test_GetLanguageConcept(t *testing.T) {
-	c, err := GetLanguageConcept("go", "arrays", nil)
+	_, err := SearchConcepts(url.Values{"q": {"arrays"}})
 	if err != nil {
 		t.Fatal(err)
-	} else {
-		t.Log(c)
 	}
 }
--- language_test.go
+++ language_test.go
@@ -1,67 +1,100 @@
 package gosyntaxdb
 
 import (
+	"math/rand"
 	"net/url"
+	"strconv"
 	"testing"
+	"time"
 )
 
-// TODO: Write better tests.
+var permalinks = []string{"java", "c", "cpp", "csharp", "python", "ruby", "javascript", "swift", "go"}
 
 func Test_GetLanguages(t *testing.T) {
+	sort := []string{"language_name", "position", "language_permalink", "language_version",
+		"-language_name", "-position", "-language_permalink", "-language_version"}
 	v := url.Values{}
-	v.Set("sort", "-1")
-	l, err := GetLanguages(v)
+
+	r := rand.New(rand.NewSource(time.Now().Unix()))
+
+	v.Set("sort", sort[r.Intn(len(sort)-1)])
+
+	_, err := GetLanguages(v)
 	if err != nil {
 		t.Fatal(err)
-	} else {
-		t.Log(l)
 	}
 }
 
 func Test_GetLanguage(t *testing.T) {
-	l, err := GetLanguage("go", nil)
+	r := rand.New(rand.NewSource(time.Now().Unix()))
+
+	_, err := GetLanguage(permalinks[r.Intn(len(permalinks)-1)], nil)
 	if err != nil {
 		t.Fatal(err)
-	} else {
-		t.Log(l)
 	}
 }
 
 func Test_GetLanguageCategories(t *testing.T) {
-	l, err := GetLanguageCategories("go", nil)
+	sort := []string{"category_name", "category_search", "language_id", "language_permalink", "position",
+		"-category_name", "-category_search", "-language_id", "-language_permalink", "-position"}
+	r := rand.New(rand.NewSource(time.Now().Unix()))
+	v := url.Values{}
+
+	v.Set("limit", strconv.Itoa(1+r.Intn(20)))
+	v.Set("sort", sort[r.Intn(len(sort)-1)])
+
+	_, err := GetLanguageCategories(permalinks[r.Intn(len(permalinks)-1)], v)
 	if err != nil {
 		t.Fatal(err)
-	} else {
-		t.Log(l)
 	}
 }
 
 func Test_GetLanguageCategoryConcepts(t *testing.T) {
-	c, err := GetLanguageCategoryConcepts("go", 50, nil)
+	sort := []string{"id", "concept_name", "category_id", "position", "language_id", "language_permalink",
+		"concept_search", "concept_permalink", "description", "syntax", "notes", "examples", "keywords", "related", "documentation",
+		"-id", "-concept_name", "-category_id", "-position", "-language_id", "-language_permalink",
+		"-concept_search", "-concept_permalink", "-description", "-syntax", "-notes", "-examples", "-keywords", "-related", "-documentation"}
+
+	categoryID := []int{50, 51, 52, 53}
+
+	r := rand.New(rand.NewSource(time.Now().Unix()))
+
+	v := url.Values{}
+	v.Set("limit", strconv.Itoa(1+r.Intn(20)))
+	v.Set("sort", sort[r.Intn(len(sort)-1)])
+
+	_, err := GetLanguageCategoryConcepts("go", categoryID[r.Intn(len(categoryID))], v)
 	if err != nil {
 		t.Fatal(err)
-	} else {
-		t.Log(c)
 	}
 }
 
 func Test_GetLanguageConcepts(t *testing.T) {
-	c, err := GetLanguageConcepts("go", nil)
+	sort := []string{"id", "concept_name", "category_id", "position", "language_id", "language_permalink",
+		"concept_search", "concept_permalink", "description", "syntax", "notes", "examples", "keywords", "related", "documentation",
+		"-id", "-concept_name", "-category_id", "-position", "-language_id", "-language_permalink",
+		"-concept_search", "-concept_permalink", "-description", "-syntax", "-notes", "-examples", "-keywords", "-related", "-documentation"}
+
+	r := rand.New(rand.NewSource(time.Now().Unix()))
+
+	v := url.Values{}
+	v.Set("limit", strconv.Itoa(1+r.Intn(20)))
+	v.Set("sort", sort[r.Intn(len(sort)-1)])
+
+	_, err := GetLanguageConcepts(permalinks[r.Intn(len(permalinks)-1)], v)
 	if err != nil {
 		t.Fatal(err)
-	} else {
-		t.Log(c)
 	}
 }
 
 func Test_SearchLanguageConcepts(t *testing.T) {
+	r := rand.New(rand.NewSource(time.Now().Unix()))
+
 	v := url.Values{}
 	v.Set("q", "array")
-	c, err := SearchLanguageConcepts("go", v)
+	_, err := SearchLanguageConcepts(permalinks[r.Intn(len(permalinks)-1)], v)
 
 	if err != nil {
 		t.Fatal(err)
-	} else {
-		t.Log(c)
 	}
 }
